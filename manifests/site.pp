0fa2471f26992a48b62eceeb56201b91e0e196aa
leotarafa <leo.tarafa@taylorandfrancis.com>
2016-06-27 18:12:54 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -36,7 +36,7 @@ ini_setting { 'random ordering':
 # The default node definition matches any node lacking a more specific node
 # definition. If there are no other nodes in this file, classes declared here
 # will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node..
+# specified in the console for that node...
 
 node default {
   # This is where you can declare classes for all nodes.
