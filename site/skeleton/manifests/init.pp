24acf7fefe94b74c213c367f7b69717bc1333845
leotarafa <leo.tarafa@taylorandfrancis.com>
2016-06-28 17:29:22 -0400

    Adding Skeleton Class

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -1,11 +1,18 @@
 class skeleton {
-  ensure => present,
-  }
+
   file { '/etc/skel':
-  ensure => directory,
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755',
   }
+  
+  # skeleton/files/bashrc
   file { '/etc/skel/.bashrc':
-  ensure => file,
-  source => 'puppet:///modules/skel/.bashrc',
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    source => 'puppet:///modules/skeleton/bashrc'
   }
 }
