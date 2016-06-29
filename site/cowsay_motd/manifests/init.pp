670199405344246fc7d15ac6c2ee007ee1fa3ea8
leotarafa <leo.tarafa@taylorandfrancis.com>
2016-06-29 10:56:43 -0400

    Creating class cowsay_motd
    This class adds output of Cowsay to /etc/MOTD.

---
+++ site/cowsay_motd/manifests/init.pp
@@ -0,0 +1,8 @@
+## Writes Cowsay command to MOTD
+	class cowsay_motd {
+		$cowsay_cmd = "/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd"
+		$cowsay_unless = '/bin/grep -F "Welcome to " /etc/motd'
+		exec {'cowsay_motd':
+			command => $cowsay_cmd,
+			unless => $cowsay_unless,
+		}
