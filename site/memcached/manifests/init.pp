6d126e0f6b72648ea04ba6618ffc8f514a6c22d6
leotarafa <leo.tarafa@taylorandfrancis.com>
2016-06-29 13:14:45 -0400

    Update init.pp

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -6,11 +6,11 @@ class memcached {
     ensure => file,
     path => '/etc/sysconfig/memcached',
     source => 'puppet:///modules/memcached/memcached'
-    require => Package['memcached'],
+    require => Package['memcached']
     }
   service { 'memcached':
     ensure => running,
     enable => true,
-    subscribe => File['memcached'],
+    subscribe => File['memcached']
     }
   }
