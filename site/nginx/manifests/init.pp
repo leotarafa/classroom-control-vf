aefc48bd129b31b895da8f8019468e9799989922
leotarafa <leo.tarafa@taylorandfrancis.com>
2016-06-29 14:12:47 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -5,22 +5,34 @@ class nginx {
   file { 'default.conf':
     ensure => file,
     path => '/etc/nginx/conf.d/default.conf',
+    owner => 'root,
+    group => 'root',
+    mode => '0664',
     source => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
     }
   file { 'nginx.conf':
     ensure => file,
     path => '/etc/nginx/nginx.conf',
+    owner => 'root,
+    group => 'root',
+    mode => '0664',
     source => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
     }
   file { 'var/www':
     ensure => directory,
     path => '/var/www',
+    owner => 'root,
+    group => 'root',
+    mode => '0775',
     }
   file { 'index.html':
     ensure => file,
     path => '/var/www/index.html',
+    owner => 'root,
+    group => 'root',
+    mode => '0664',
     source => 'puppet:///modules/nginx/index.html'
     }
   service { 'nginx':
