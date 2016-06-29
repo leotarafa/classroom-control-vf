2e0465d0ea3aef19bce57b0e046e55389984c503
leotarafa <leo.tarafa@taylorandfrancis.com>
2016-06-29 11:34:50 -0400

    Update init.pp

--- site/cowsay_motd/manifests/init.pp
+++ site/cowsay_motd/manifests/init.pp
@@ -1,8 +1,9 @@
 ## Writes Cowsay command to MOTD
-	class cowsay_motd {
-		$cowsay_cmd = "/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd"
-		$cowsay_unless = '/bin/grep -F "Welcome to " /etc/motd'
-		exec {'cowsay_motd':
-			command => $cowsay_cmd,
-			unless => $cowsay_unless,
+class cowsay_motd {
+$cowsay_cmd = "/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd"
+$cowsay_unless = '/bin/grep -F "Welcome to " /etc/motd'
+exec {'cowsay_motd':
+		command => $cowsay_cmd,
+		unless => $cowsay_unless,
 		}
+	}
