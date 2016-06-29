607736c4f7bc24cfc7f99170a7a61ef32d88907a
Leo <leo.tarafa@taylorandfrancis.com>
2016-06-29 15:51:52 -0400

    adding resource defaults for file details

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,46 +1,39 @@
 class nginx {
   package { 'nginx':
     ensure => latest,
-    }
+  }
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+  }
   file { 'default.conf':
-    ensure => file,
-    path => '/etc/nginx/conf.d/default.conf',
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/default.conf',
+    ensure  => file,
+    path    => '/etc/nginx/conf.d/default.conf',
+    source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
-    }
+  }
   file { 'nginx.conf':
-    ensure => file,
-    path => '/etc/nginx/nginx.conf',
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
-    source => 'puppet:///modules/nginx/nginx.conf',
+    ensure  => file,
+    path    => '/etc/nginx/nginx.conf',
+    source  => 'puppet:///modules/nginx/nginx.conf',
     require => Package['nginx'],
-    }
+  }
   file { 'var/www':
     ensure => directory,
-    path => '/var/www',
-    owner => 'root',
-    group => 'root',
-    mode => '0775',
-    }
+    path   => '/var/www',
+  }
   file { 'index.html':
     ensure => file,
-    path => '/var/www/index.html',
-    owner => 'root',
-    group => 'root',
-    mode => '0664',
+    path   => '/var/www/index.html',
     source => 'puppet:///modules/nginx/index.html'
-    }
+  }
   service { 'nginx':
-    ensure => running,
-    enable => true,
+    ensure    => running,
+    enable    => true,
     subscribe => [
       File['nginx.conf'],
       File['default.conf'],
-      ]
-    }
+    ]
+  }
 }
