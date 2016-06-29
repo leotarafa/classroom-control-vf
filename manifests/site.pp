78ff9b49c9904b38356136e7cd237bf1258f4695
leotarafa <leo.tarafa@taylorandfrancis.com>
2016-06-29 11:19:33 -0400

    Adding node definition for leotarafa.puppetlabs.vm

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,25 +1,12 @@
 ## site.pp ##
 
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
-
 ## Active Configurations ##
 
-# PRIMARY FILEBUCKET
-# This configures puppet agent and puppet inspect to back up file contents when
-# they run. The Puppet Enterprise console needs this to display file contents
-# and differences.
 
-# Disable filebucket by default for all File resources:
+## Disable filebucket by default for all File resources:
 File { backup => false }
 
-# Randomize enforcement order to help understand relationships
+## Randomize enforcement order to help understand relationships
 ini_setting { 'random ordering':
   ensure  => present,
   path    => "${settings::confdir}/puppet.conf",
@@ -28,31 +15,23 @@ ini_setting { 'random ordering':
   value   => 'title-hash',
 }
 
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node...
-
+## DEFAULT NODE
 node default {
   include users
   include skeleton
-  notify { "Hello, my name is ${::hostname}": }
-
-# file { 'motd':
-#  ensure => file,
-#  path => "/etc/motd",
-#  content => "Smile, it makes people wonder!"
-#  }
-  $cowsay_cmd = "/usr/local/bin/cowsay 'Welcome to ${::fqdn}!' > /etc/motd"
-  $cowsay_unless = '/bin/grep -F "Welcome to" /etc/motd'
-  
-  exec { 'cowsay_motd':
-    command => $cowsay_cmd,
-    unless  => $cowsay_unless,
+  include cowsay_motd
+  include notify
+#Node definition for leotarafa.puppetlabs.vm
+node leotarafa.puppetlabs.vm {
+  include users
+  include skeleton
+  include notify
+  include cowsay_motd
+  host {'testing.puppetlabs.vm':
+    comment => "This file is modified by Puppet. Manual changes will be overwritten.",
+    ip => '127.0.0.1',
+    ensure => present,
+    host_aliases => 'testing',
   }
 }
 
