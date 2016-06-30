31509d32f90e690aea170106cf518b303d2a43f4
Leo <leo.tarafa@taylorandfrancis.com>
2016-06-30 12:04:49 -0400

    Managing nginx with a template

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,39 +1,58 @@
 class nginx {
-  package { 'nginx':
-    ensure => latest,
+  case $::osfamily {
+    'redhat','debian' : {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir = '/var/log/nginx'
+    }
+    'windows' : {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx'
+      $logdir = 'C:/ProgramData/nginx/logs'
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${::osfamily}")
+    }
+  }
+  # user the service will run as. Used in the nginx.conf.erb template
+  $user = $::osfamily ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
   }
   File {
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
+    owner => $owner,
+    group => $group,
+    mode  => '0664',
   }
-  file { 'default.conf':
-    ensure  => file,
-    path    => '/etc/nginx/conf.d/default.conf',
-    source  => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
+  package { $package:
+    ensure => present,
   }
-  file { 'nginx.conf':
-    ensure  => file,
-    path    => '/etc/nginx/nginx.conf',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-  }
-  file { 'var/www':
+  file { [ $docroot, "${confdir}/conf.d" ]:
     ensure => directory,
-    path   => '/var/www',
   }
-  file { 'index.html':
+  file { "${docroot}/index.html":
     ensure => file,
-    path   => '/var/www/index.html',
-    source => 'puppet:///modules/nginx/index.html'
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  file { "${confdir}/nginx.conf":
+    ensure  => file,
+    content => template('nginx/nginx.conf.erb'),
+    notify  => Service['nginx'],
+  }
+  file { "${confdir}/conf.d/default.conf":
+    ensure  => file,
+    content => template('nginx/default.conf.erb'),
+    notify  => Service['nginx'],
   }
   service { 'nginx':
-    ensure    => running,
-    enable    => true,
-    subscribe => [
-      File['nginx.conf'],
-      File['default.conf'],
-    ]
+    ensure => running,
+    enable => true,
   }
 }
